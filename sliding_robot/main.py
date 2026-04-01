7c38d912cd2d618f08114b8e5ec4bb4924b3078e
Sebastian Tilaguy Lezama <sebastian.tilaguy@gmail.com>
2026-04-01 14:43:20 -0500

    refactor: clean up unused variables and improve simulation parameters

--- sliding_robot/main.py
+++ sliding_robot/main.py
@@ -9,7 +9,6 @@ import matplotlib.colors as mcolors
 from collections import deque
 
 from Dynamic_model import PulleyDynamic2D, PulleyDynamic_Lagrange2D
-# from Control_logic import PositionCascade2D, AttitudeControl2D
 from Geometrical_functios import FormationManager
 
 Ts = 0.02
@@ -19,28 +18,12 @@ dt = Ts / sub_steps
 num_robots = 2
 load_mass = 0.027
 
-# ================= CRAZYFLIE 2.1 CONFIGURATION =================
-d = 0.046
-max_rpm = 22000.0
-kf_rpm = 1.28e-8
-km_rpm = 2.0e-10
-motor_gain = 18.0
-robot_mass = 0.027
-robot_inertia = 1.4e-5
-
-robot_masses = np.full(num_robots, robot_mass)
-# robot_inertia = [robot_inertia] * num_robots
 init_load_pos = [0.0, 1.0]
 init_cable_len = 4
-# s = 7 * d
-# print(f"safe distance: {s} m")
 
 # ================= PHYSICAL INITIALIZATION =================
 pulley = PulleyDynamic2D(mass=load_mass, pulley_pos=np.array(init_load_pos), cable_len=init_cable_len, Ts=dt, damping=0.025)
 pulley2 = PulleyDynamic_Lagrange2D(mass=load_mass, pulley_pos=np.array(init_load_pos), cable_len=init_cable_len, Ts=dt, damping=0.025)
-# all_masses = np.insert(robot_masses, 0, load_mass)
-# sys = SystemDynamic(num_robots=num_robots, agent_masses=list(all_masses), robot_inertia=robot_inertia)
-# sys.Ts = dt
 edges=[(0, 1), (0, 2)]
 print(f"Edges set: {edges}")
 
@@ -50,44 +33,14 @@ print(f"Cables length: {cables_list}")
 positions_ref = pos_manager.generate_initial_position(load_position=init_load_pos, base_sep=init_cable_len*0.9)
 # pulley.x = np.array(positions_ref[1]).reshape(2,1)
 
-# for i, pos in enumerate(positions_ref):
-#   idx, _ = sys.get_agent_indices(i)
-#   sys.x[idx : idx + 2, 0] = pos
-
-# motors, pos_ctrls, att_ctrls = [], [], []
-# max_w_h = 0.0
-# for i in range(num_robots):
-#   hover_mass = robot_masses[i]
-#   mot = MotorDynamics2D(L=d, max_omega_rpm=max_rpm, motor_gain=motor_gain, kf_in_rpm=kf_rpm)
-#   w_h = mot.set_hover_from_mass(hover_mass)
-
-#   if max_w_h < w_h:
-#     max_w_h = w_h
-
-#   mot.omega[:] = w_h
-#   motors.append(mot)
-
-#   pos_ctrls.append(PositionCascade2D(
-#     kp_x=2.3, ki_x=1.5, kd_x=1.5,
-#     kp_y=12.0, ki_y=8.6, kd_y=6.0,
-#     ax_limit=2.0, ay_limit=2.0,
-#     L=d, kf=mot.kf, omega_hover=w_h))
-
-#   att_ctrls.append(AttitudeControl2D(
-#     L=d, kf=mot.kf, omega_hover=w_h,
-#     kp_theta=0.12, kd_theta=0.03))
-
-# max_w_h = max_w_h * 60 / (2*np.pi)
-# print(f"Maximum ω_h = {max_w_h} [rad/s]")
-
 # ================= DASHBOARD UI =================
 fig = plt.figure(figsize=(10, 6))
-gs = GridSpec(4, 2, width_ratios=[1, 1.2])
+gs = GridSpec(2, 2, width_ratios=[1, 1.2])
 
 # 1. Simulation View (Left)
 ax_sim = fig.add_subplot(gs[:, 0])
-ax_sim.set_xlim(-3, 3)
-ax_sim.set_ylim(0, 5)
+ax_sim.set_xlim(-5, 5)
+ax_sim.set_ylim(0, 10)
 ax_sim.set_aspect('equal')
 ax_sim.grid(True, alpha=0.3)
 
@@ -104,51 +57,23 @@ pt_load, = ax_sim.plot([], [], 'ro', markersize=10, zorder=5)
 
 # --- SLIDERS ---
 _, x1, x2 = positions_ref
-ax1_sx = plt.axes([0.1, 0.17, 0.3, 0.02]) # type: ignore
-s_ref_x1 = Slider(ax1_sx, 'D1 X Ref', -2.0, 2.0, valinit=x1[0])
+ax1_sx = plt.axes([0.1, 0.13, 0.3, 0.02]) # type: ignore
+s_ref_x1 = Slider(ax1_sx, 'D1 X Ref', -5.0, 5.0, valinit=x1[0])
 
-ax1_sy = plt.axes([0.1, 0.14, 0.3, 0.02]) # type: ignore
-s_ref_y1 = Slider(ax1_sy, 'D1 Y Ref', 0.0, 4.0, valinit=x1[1])
+ax1_sy = plt.axes([0.1, 0.105, 0.3, 0.02]) # type: ignore
+s_ref_y1 = Slider(ax1_sy, 'D1 Y Ref', 0.0, 10.0, valinit=x1[1])
 
-ax2_sx = plt.axes([0.1, 0.11, 0.3, 0.02]) # type: ignore
-s_ref_x2 = Slider(ax2_sx, 'D2 X Ref', -2.0, 2.0, valinit=x2[0])
+ax2_sx = plt.axes([0.1, 0.08, 0.3, 0.02]) # type: ignore
+s_ref_x2 = Slider(ax2_sx, 'D2 X Ref', -5.0, 5.0, valinit=x2[0])
 
-ax2_sy = plt.axes([0.1, 0.08, 0.3, 0.02]) # type: ignore
-s_ref_y2 = Slider(ax2_sy, 'D2 Y Ref', 0.0, 4.0, valinit=x2[1])
-
-ax_si = plt.axes([0.1, 0.05, 0.3, 0.02]) # type: ignore
-s_robot_idx = Slider(ax_si, 'Show Robot', 1, num_robots, valinit=0, valstep=1)
+ax2_sy = plt.axes([0.1, 0.05, 0.3, 0.02]) # type: ignore
+s_ref_y2 = Slider(ax2_sy, 'D2 Y Ref', 0.0, 10.0, valinit=x2[1])
 
 ax_sp = plt.axes([0.1, 0.02, 0.3, 0.02], facecolor='mistyrose') # type: ignore
-s_perturb = Slider(ax_sp, 'PERTURB X (N)', -8, 8, valinit=0.0)
+s_perturb = Slider(ax_sp, 'DISTURB X (N)', -8, 8, valinit=0.0)
 
 # Telemetry (Right)
-max_omega = (max_rpm * 2.0 * math.pi) / 60.0
-ax_rpm = fig.add_subplot(gs[0, 1])
-# l_rpm1, = ax_rpm.plot([], [], 'r', label='M1')
-# l_rpm2, = ax_rpm.plot([], [], 'b', label='M2')
-# l_rpm_ref, = ax_rpm.plot([], [], '--k', label='Hover ref', alpha=0.4)
-# ax_rpm.set_ylabel('RPM')
-# ax_rpm.legend(loc='upper right', fontsize='x-small')
-# ax_rpm.set_ylim(max_w_h * 0.8, max(max_omega * 1.2, max_w_h * 1.2))
-
-ax_pos = fig.add_subplot(gs[1, 1], sharex=ax_rpm)
-# l_x_act, = ax_pos.plot([], [], 'b', label='Robot X')
-# l_x_ref, = ax_pos.plot([], [], '--b', label='X ref', alpha=0.4)
-# l_z_act, = ax_pos.plot([], [], 'g', label='Robot Y')
-# l_z_ref, = ax_pos.plot([], [], '--g', label='Y ref', alpha=0.4)
-# ax_pos.set_ylabel('Robot Pos [m]')
-# ax_pos.legend(loc='upper right', fontsize='x-small')
-# ax_pos.set_ylim(-4, 6)
-
-ax_phi = fig.add_subplot(gs[2, 1], sharex=ax_rpm)
-# l_phi_ref, = ax_phi.plot([], [], 'k--', alpha=0.4)
-# l_phi_act, = ax_phi.plot([], [], 'orange', label='Phi')
-# ax_phi.set_ylabel('Phi [rad]')
-# ax_phi.legend(loc='upper right', fontsize='x-small')
-# ax_phi.set_ylim(-1.04, 1.04)
-
-ax_load = fig.add_subplot(gs[3, 1], sharex=ax_rpm)
+ax_load = fig.add_subplot(gs[0, 1])
 l_load_x_ref, = ax_load.plot([], [], 'r--', alpha=0.4, label='Ref X')
 l_load_x_act, = ax_load.plot([], [], 'r', lw=1.5, label='Load X')
 l_load_y_ref, = ax_load.plot([], [], 'g--', alpha=0.4, label='Ref Y')
@@ -157,18 +82,11 @@ ax_load.set_ylabel('Load Pos [m]')
 ax_load.legend(loc='upper right', fontsize='x-small')
 ax_load.set_xlabel('Time [s]')
 ax_load.set_ylim(-4, 6)
-
-for ax in [ax_rpm, ax_pos, ax_phi, ax_load]:
-  ax.grid(True, linestyle=':', alpha=0.6)
+ax_load.grid(True, linestyle=':', alpha=0.6)
 
 # Buffers
 history_len = 250
 t_d = deque(maxlen=history_len)
-# rpm_d = [deque(maxlen=history_len) for _ in range(2)]
-# rpm_ref_d = deque(maxlen=history_len)
-# pos_d = [deque(maxlen=history_len) for _ in range(2)]
-# pos_ref = [deque(maxlen=history_len) for _ in range(2)]
-# phi_d = [deque(maxlen=history_len) for _ in range(2)]
 
 load_d = {
   'x_ref': deque(maxlen=history_len),
@@ -178,76 +96,25 @@ load_d = {
 }
 
 time_elapsed = 0.0
-# parent_of  = {child: parent for parent, child in sys.edges}
 
 def update(frame):
   global time_elapsed, positions_ref
 
   t1 = np.array([s_ref_x1.val, s_ref_y1.val])
   t2 = np.array([s_ref_x2.val, s_ref_y2.val])
-  ridx = int(s_robot_idx.val) - 1
   f_perturb = s_perturb.val  # Capture impulse value
-
-  temp_data = {}
-
-#   T_final = np.zeros(num_robots)
-#   L_list = np.zeros(num_robots)
 
   positions_ref = [positions_ref[0], t1, t2]
   _, x1, x2 = positions_ref
-  for step_idx in range(sub_steps):
-#     force_list, torque_list = [], []
-
+  for _ in range(sub_steps):
     pulley.step(x1=x1.reshape((2,1)), x2=x2.reshape((2,1)), F_ext=f_perturb)
-
-#     for i in range(num_robots):
-#       idx, _ = sys.get_agent_indices(i + 1)
-#       x, y, phi = sys.x[idx:idx+3, 0]
-#       vx, vz, dphi = sys.d_q[idx:idx+3, 0]
-
-#       rx = positions_ref[i+1][0] + tx
-#       ry = positions_ref[i+1][1] + (ty - init_load_pos[1])
-
-#       # for _ in range(sub_steps):
-#       _, _, p_des, Dw_F = pos_ctrls[i].step(
-#         xd=rx, x=x, vx=vx,
-#         yd=ry, y=y, vy=vz,
-#         m=robot_masses[i],
-#         dt=dt)
-
-#       p_des = np.clip(p_des, -0.6, 0.6)
-
-#       Dw_phi = att_ctrls[i].attitude_channels(p_des, phi, dphi)
-
-#       w_real, F, _ = motors[i].update(
-#         dt,
-#         Dw=(np.clip(Dw_F, -0.8*motors[i].omega_h, 0.8*motors[i].omega_h), Dw_phi)
-#       )
-
-#       force_list.append(float(np.sum(F)))
-#       torque_list.append(d * (F[1] - F[0]))
-
-#       if step_idx == sub_steps - 1:
-#         i_rho = parent_of[i + 1]
-#         idx_rho, _ = sys.get_agent_indices(i_rho)
-#         x_rho, y_rho = sys.x[idx_rho:idx_rho+2, 0]
-#         L_list[i] = np.sqrt((x - x_rho) ** 2 + (y - y_rho) ** 2)
-
-#         if i == (ridx - 1):
-#           temp_data['rpm'] = w_real * 60 / (2*np.pi)
-#           temp_data['rpm_h'] = motors[i].omega_h * 60 / (2*np.pi)
-#           temp_data['pos'] = [x, y]
-#           temp_data["pos_ref"] = [rx, ry]
-#           temp_data['phi'] = [p_des, phi]
-
-#     _, lambdas = sys.step(F=force_list, τ=torque_list, W_load=f_perturb, safe_dist=s)
 
     time_elapsed += dt
 
-#   t_val = np.abs(T_final[ridx - 1])
-#   l_val = L_list[ridx - 1]
-  stats_text = (f"► ROBOT {ridx} STATS\n"
-                f"Length: {pulley.l[ridx]:.3f} [m]")
+  stats_text = (f"► STATS\n"
+                f"δl_1: {pulley.l[0]:.3f} [m]\n"
+                f"δl_2: {pulley.l[1]:.3f} [m]\n"
+                f"LENGTH: {sum(pulley.l):.3f} [m]")
 
   text_telemetry.set_text(stats_text)
 
@@ -257,15 +124,6 @@ def update(frame):
 
   # Update history buffers
   t_d.append(time_elapsed)
-#   rpm_d[0].append(temp_data['rpm'][0])
-#   rpm_d[1].append(temp_data['rpm'][1])
-#   rpm_ref_d.append(temp_data['rpm_h'])
-#   pos_d[0].append(temp_data['pos'][0])
-#   pos_d[1].append(temp_data['pos'][1])
-#   pos_ref[0].append(temp_data['pos_ref'][0])
-#   pos_ref[1].append(temp_data['pos_ref'][1])
-#   phi_d[0].append(temp_data['phi'][0])
-#   phi_d[1].append(temp_data['phi'][1])
 
   load_d['x_ref'].append(pulley.x[0,0])
   load_d['x_act'].append(pulley.x[0,0])
@@ -273,18 +131,13 @@ def update(frame):
   load_d['y_act'].append(pulley.x[1,0])
 
   # Redraw Telemetry
-#   l_rpm1.set_data(t_d, rpm_d[0]); l_rpm2.set_data(t_d, rpm_d[1])
-#   l_rpm_ref.set_data(t_d, rpm_ref_d)
-#   l_x_act.set_data(t_d, pos_d[0]); l_z_act.set_data(t_d, pos_d[1])
-#   l_x_ref.set_data(t_d, pos_ref[0]); l_z_ref.set_data(t_d, pos_ref[1])
-#   l_phi_ref.set_data(t_d, phi_d[0]); l_phi_act.set_data(t_d, phi_d[1])
   l_load_x_ref.set_data(t_d, load_d['x_ref']); l_load_x_act.set_data(t_d, load_d['x_act'])
   l_load_y_ref.set_data(t_d, load_d['y_ref']); l_load_y_act.set_data(t_d, load_d['y_act'])
-  ax_rpm.set_xlim(max(0, time_elapsed - 5), time_elapsed + 0.1)
+  ax_load.set_xlim(max(0, time_elapsed - 5), time_elapsed + 0.1)
 
   # Visual Simulation (Drones as lines)
   dx, dy = [], []
-  visual_arm = d * 2
+  visual_arm = 0.1
   for i in range(1, 2 + 1):
     phic = 0.0
     xc, yc = positions_ref[i]
@@ -302,7 +155,7 @@ def update(frame):
 
   cable_collection.set_segments(segments)
 
-  T_abs = np.abs(pulley.T) # type: ignore
+  T_abs = np.abs(pulley.T)
   t_min = T_abs.min()
   t_max = max(T_abs.max(), 1e-3)
 
@@ -311,7 +164,6 @@ def update(frame):
 
   pt_load.set_data(pulley.x)
 
-  # return cable_collection, line_drones, pt_load, l_rpm1, l_x_act, l_load_x_act, text_telemetry
   return cable_collection, line_drones, pt_load, l_load_x_act, text_telemetry
 
 ani = FuncAnimation(fig, update, frames=None, interval=Ts*1000, blit=False, cache_frame_data=False)
